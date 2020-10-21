2e54e5c2733bd0911464743a9933ac8c0799481d
Christian Baker <christianbaker1998@gmail.com>
2020-10-21 16:18:36 -0400

    Got Classes pretty much done, time to input some code

--- PRSC2/Controllers/RequestsController.cs
+++ PRSC2/Controllers/RequestsController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
 using PRSC2.Data;
 using PRSC2.Models;
 
@@ -52,6 +53,7 @@ namespace PRSC2.Controllers
             if (id != request.Id)
             {
                 return BadRequest();
+
             }
 
             _context.Entry(request).State = EntityState.Modified;
@@ -82,7 +84,7 @@ namespace PRSC2.Controllers
         public async Task<ActionResult<Request>> PostRequest(Request request)
         {
 
-            
+
             _context.Request.Add(request);
             await _context.SaveChangesAsync();
 
@@ -109,5 +111,44 @@ namespace PRSC2.Controllers
         {
             return _context.Request.Any(e => e.Id == id);
         }
+        [HttpGet("Requests")]
+        public async Task<ActionResult<IEnumerable<Request>>> GetReviews()
+        {
+            return await _context.Request.ToListAsync();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Request>>> ApproveRequest()
+        {
+            return await _context.Request.ToListAsync();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Request>>> RejectRequest()
+        {
+            return await _context.Request.ToListAsync();
+        }
+        public string Status { get; set; }
+
+        public bool ReviewRrequest(Request request)
+        {
+            if (request.Total <= 50)
+            {
+                request.Status = "APPROVED";
+            }
+            else
+            {
+                request.Status = "REVIEW";
+            }
+            _context.SaveChanges();
+            return true;
+
+        }
+        public bool SetToRejected(Request request)
+        {
+            request.Status = "DENIED";
+            _context.SaveChanges();
+            return true;
+        }
     }
 }
+
+
